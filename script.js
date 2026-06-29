ddd2f0579991a7d159467f0e1dd7cf8b2eaa9400
Afnan Saqib <afnansaqib52@gmail.com>
2026-06-29 17:10:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-
 const form = document.querySelector("form");
 const tableBody = document.querySelector("tbody");
 
-
-
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-
     const inputs = document.querySelectorAll("form input");
-
     let name = inputs[0].value;
     let sap = inputs[1].value;
     let math = Number(inputs[2].value);
     let cs = Number(inputs[3].value);
     let english = Number(inputs[4].value);
-
     let total = math + cs + english;
-
     let grade = "";
 
     if (total >= 240) {
@@ -31,9 +24,7 @@ form.addEventListener("submit", function (e) {
         grade = "Fail";
     }
 
-   
     let row = document.createElement("tr");
-
     row.innerHTML = `
         <td>${name}</td>
         <td>${sap}</td>
@@ -44,16 +35,11 @@ form.addEventListener("submit", function (e) {
         <td>${grade}</td>
         <td><button class="delete-btn">Delete</button></td>
     `;
-
     tableBody.appendChild(row);
-
     form.reset();
 });
 
-
-
 tableBody.addEventListener("click", function (e) {
-
     if (e.target.classList.contains("delete-btn")) {
         e.target.parentElement.parentElement.remove();
     }
